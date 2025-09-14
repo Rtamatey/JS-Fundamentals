db189866cef3f6e368d9236d1a92b863bdd65a61
Ransford Owreku Tamatey <rtamatey0925@gmail.com>
2025-09-14 21:04:41 +0000

    Add script that prints messages based on arguments

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+
+// Get the command line arguments excluding the first two default ones
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
