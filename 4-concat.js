5acb55a882e3b784e950f2ab7f0d19f81e4d03f8
Ransford Owreku Tamatey <rtamatey0925@gmail.com>
2025-09-15 15:33:24 +0000

    Fix 4-concat.js: correct argument handling and update README

---
+++ 4-concat.js
@@ -0,0 +1,11 @@
+
+const a = process.argv[2];
+const b = process.argv[3];
+
+if (a === undefined && b === undefined) {
+  console.log("0 argument");
+} else if (b === undefined) {
+  console.log(a);
+} else {
+  console.log(a + ' ' + b);
+}
