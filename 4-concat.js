0f8454db88e787ec4829dc6f08a985e795fe01ba
Ransford Owreku Tamatey <rtamatey0925@gmail.com>
2025-09-15 15:44:47 +0000

    Fix 4-concat.js: match grader cases for 0/1/2 args; update README

--- 4-concat.js
+++ 4-concat.js
@@ -3,7 +3,7 @@ const a = process.argv[2];
 const b = process.argv[3];
 
 if (a === undefined && b === undefined) {
-  console.log("0 argument");
+  console.log('0 argument');
 } else if (b === undefined) {
   console.log(a);
 } else {
